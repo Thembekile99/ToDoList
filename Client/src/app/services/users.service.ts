0382cabf11aeb81a5d08c0ebb385280c1b525d82
Thembekile99 <missgracemgabi@gmail.com>
2023-12-01 16:13:33 +0200

    service

--- Client/src/app/services/users.service.ts
+++ Client/src/app/services/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../interface/user'
+import { Task } from '../model/task';
 
 @Injectable({
   providedIn: 'root'
@@ -20,6 +21,16 @@ register(user:User): Observable<User> {
 login(user:User): Observable<User> {
   return this.http.post<User>(`${this.baseUrl}/login`, user
 );
+} 
+
+addTask(task:Task): Observable<Task> {
+  return this.http.post<Task>(`${this.baseUrl}/addtask`,task);
 }
 
+getallTask(task:Task): Observable<Task[]> {
+  return this.http.post<Task[]>(`${this.baseUrl}/addtask`,task);
+}
+deleteTask(task:Task): Observable<Task> {
+  return this.http.post<Task>(`${this.baseUrl}/addtask`,task);
+}
 }
